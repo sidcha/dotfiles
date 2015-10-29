9265216a85624c85a8aece38e36ef08a61a0ac94
Siddharth Chandrasekaran <siddharth@embedjournal.com>
2015-10-29 11:22:31 +0530

    Auto deploy script in place

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Siddharth Chandrasekaran's Linux Customizations
 Change Log:
 Wed Oct 28 11:06:35 IST 2015 - Added new file structure
 Wed Oct 28 11:25:56 IST 2015 - Added screen help
+Thu Oct 29 10:50:04 IST 2015 - Added version control prompt
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 DIR=$PWD
 echo "Seting up siddhart's customizations"
+
+echo "Asking vimrc to source external customization file.."
 echo ":source $DIR/vim/vimrc" > ~/.vimrc
+
+echo "Asking bashrc to source external customization file.."
+echo "source $DIR/bash/bashrc" > ~/.bashrc
+
+mkdir -p ~/bin
+echo "Adding vcprompt executable to local bin"
+cp git/vcprompt ~/bin/vcprompt
+
+echo "Resourcing bashrc"
+. ~/.bashrc
 
 echo "All Done"
