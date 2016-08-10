8b8f5df87f604dac59fc333cc07f2659ebd1cf1e
Siddharth Chandrasekaran <siddharth3141@gmail.com>
2016-08-11 01:56:52 +0530

    Deploy script enhanced

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
-DIR=$PWD
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle 
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+DIR=`git rev-parse --show-toplevel`
 
-cd ~/.vim/bundle
-while IFS='' read -r line || [[ -n "$line" ]]; do
-	git clone $line
-done < ~/customizer/other/vim-plugin-git-url.list
-cd $DIR
+if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
+	mkdir -p ~/.vim/autoload ~/.vim/bundle 
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
 
-echo "Adding simlinks for dotFiles.."
-ln -f -s customizer/runcon/vimrc ~/.vimrc
-ln -f -s customizer/runcon/bashrc ~/.bashrc
-ln -f -s customizer/runcon/screenrc ~/.screenrc
-ln -f -s customizer/runcon/Xresources ~/.Xresources
+if [ ! -d ~/.vim/bundle ]; then
+	mkdir -p ~/.vim/bundle
+	cd ~/.vim/bundle
+	while IFS='' read -r line || [[ -n "$line" ]]; do
+		echo -n "Cloning from $line... "
+		git clone $line > /dev/null
+		echo "Done."
+	done < $DIR/other/vim-plugin-git-url.list
+	cd - > /dev/null
+fi
 
-cp scripts/create.pl ~/bin/
+echo -n "Adding simlinks for dotFiles... "
+ln -f -s $DIR/runcon/vimrc ~/.vimrc
+ln -f -s $DIR/runcon/bashrc ~/.bashrc
+ln -f -s $DIR/runcon/screenrc ~/.screenrc
+ln -f -s $DIR/runcon/Xresources ~/.Xresources
+echo "Done."
 
+echo -n "Adding custom scripts... "
 mkdir -p ~/bin
-#echo "Adding vcprompt executable to local bin"
-#cd scripts/vcprompt/vcprompt-1.2.1
-#make -f Makefile.in
-#make
-#cp vcprompt* ~/bin/
-#cd $DIR
+cp -f -r scripts/* ~/bin/
+echo "Done."
 
-echo "Resourcing bashrc"
-. ~/.bashrc
+echo -n "Resourcing bashrc... "
+. ~/.bashrc > /dev/null
+echo "Done."
 
 echo "All Done"
+
--- scripts/randQuote.pl
+++ scripts/randQuote.pl
@@ -4,5 +4,7 @@ my $argc = @ARGV;
 open($file, "<", "quote.list") or die "Quote list not found!\n";
 my @lines = <$file>;
 my $total = @lines;
-my $line = int (($total/100) * (rand() * 100));
-print $lines[$line];
+my $line = int (abs(rand() * $total));
+#print $lines[$line];
+
+print "$total => $line", "\n";
