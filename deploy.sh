6d558da7c311efbf0ca2a435e0a9b531a6413389
Siddharth Chandrasekaran <siddharth3141@gmail.com>
2016-12-06 13:30:39 +0530

    Modified startup scripts. Fixed bug in deployment with different folder names.

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,30 @@
 
 DIR=`git rev-parse --show-toplevel`
 
-if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
-	mkdir -p ~/.vim/autoload ~/.vim/bundle 
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+if [ ! -d ~/.vim ]; then
+	echo "Unable to find a .vim directory. Perhaps you haven't installed?"
+else
+	if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
+		echo "Downloading pathogen for vim"
+		mkdir -p ~/.vim/autoload ~/.vim/bundle 
+		curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+	fi
+
+	if [ ! -d ~/.vim/bundle ]; then
+		echo "Fectching vim plugins.."
+		mkdir -p ~/.vim/bundle
+		cd ~/.vim/bundle
+		while IFS='' read -r line || [[ -n "$line" ]]; do
+			echo -n "Cloning from $line... "
+			git clone $line > /dev/null
+			echo "Done."
+		done < $DIR/other/vim-plugin-git-url.list
+		cd - > /dev/null
+	fi
 fi
 
-if [ ! -d ~/.vim/bundle ]; then
-	mkdir -p ~/.vim/bundle
-	cd ~/.vim/bundle
-	while IFS='' read -r line || [[ -n "$line" ]]; do
-		echo -n "Cloning from $line... "
-		git clone $line > /dev/null
-		echo "Done."
-	done < $DIR/other/vim-plugin-git-url.list
-	cd - > /dev/null
+if [ ! -f ~/.env ]; then
+	echo "export CFG_SCRIPT_DIR=$DIR" > ~/.env
 fi
 
 echo -n "Adding simlinks for dotFiles... "
--- scripts/greet.sh
+++ scripts/greet.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-if [ "$BOX_NAME" = "WORK" ]; then
-	echo ""
-else
-	#cat ~/customizer/scripts/ascii.art
-	date
+cols=`tput cols`
+if [ "$cols" > "80" ]; then
+	cols=80;
 fi
 
-FILE=$(ls ~/customizer/scripts/*.list |sort -R |tail -1)
+space=`echo "($cols - 25)/2" | bc`
 
-shuf -n 1 $FILE | fold -w 75 -s
+for i in `seq $space`; do SP="$SP "; done
+echo "$SP _| o  _  |  _ __  _  |  $SP"
+echo "$SP(_| | _>  | (_||||(_) |< $SP"
+
+for i in `seq $cols`; do echo -n "-"; done; echo ""
+
+FILE=$(ls $CFG_SCRIPT_DIR/scripts/*.list |sort -R |tail -1)
+shuf -n 1 $FILE | fold -w $cols -s; echo ""
+
+dt=`date`
+ll=$(last -1 -R  $USER | head -1 | cut -c 23-)
+
+echo "Last Login: $ll"
+echo "Date: $dt"
 echo ""
