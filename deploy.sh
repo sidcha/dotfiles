65a5003ccb2421ef579a674c81f40d6c2617bb6c
Siddharth Chandrasekaran <siddharth@embedjournal.com>
2021-06-28 09:34:39 +0200

    Add git alias for logs, rebase and fixups
    Signed-off-by: Siddharth Chandrasekaran <siddharth@embedjournal.com>

--- deploy.sh
+++ deploy.sh
@@ -53,19 +53,23 @@ echo "Done."
 git config --global user.name "Siddharth Chandrasekaran"
 git config --global init.templatedir "$DIR/git_template"
 git config --global rebase.autoSquash true
+git config --global sendemail.confirm always
+git config --global am.threeWay true
+git config --global core.excludesfile ~/.gitignore
+
+# Git alias
 git config --global alias.ctags '!.git/hooks/ctags'
 git config --global alias.last 'diff HEAD^ HEAD'
 git config --global alias.su 'submodule update --recursive'
-git config --global sendemail.confirm always
-git config --global alias.r 'rebase -i HEAD~$1'
-git config --global am.threeWay true
-git config --global core.excludesfile ~/.gitignore
+git config --global alias.ll 'log --format=%h --abbrev=12 --oneline'
+git config --global alias.l '!f() { git log --format=%h --abbrev=12 --oneline origin/master..HEAD | tac | nl | tac | perl -pe "s/([0-9a-f]{12})/\\e[1;31m\\1\\e[m/"; }; f'
+git config --global alias.fixup '!f() { h="$(git rev-list --reverse origin/master..HEAD | sed -n -e ${1}p)"; git commit --fixup=$h; }; f'
+git config --global alias.rb '!f() { count=${1:-"$(git rev-list --reverse origin/master..HEAD | wc -l)"}; git rebase -i --autosquash HEAD~${count}; }; f'
 # For github PRs
 git config --global alias.pr '!f() { git fetch -fu ${2:-$(git remote |grep ^upstream || echo origin)} refs/pull/$1/head:pr/$1 && git checkout pr/$1; }; f'
 git config --global alias.pr-clean '!git for-each-ref refs/heads/pr/* --format="%(refname)" | while read ref ; do branch=${ref#refs/heads/} ; git branch -D $branch ; done'
 # For stash/bitbucket
 git config --global alias.spr '!f() { git fetch -fu ${2:-$(git remote |grep ^upstream || echo origin)} refs/pull-requests/$1/from:pr/$1 && git checkout pr/$1; }; f'
-git config --global alias.fixup '!f() { git commit --all --fixup=$1; }; f'
 
 touch ~/.ssh/config
 if ! grep -qe 'Include .*/\.files/config/ssh_config' ~/.ssh/config; then
