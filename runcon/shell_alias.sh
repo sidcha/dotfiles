d60c09aced24dc42a3adc06a8fde4df3fadd3863
Siddharth Chandrasekaran <siddharth@embedjournal.com>
2021-08-17 21:07:48 +0200

    Fix perl onliner quote escape mess
    Signed-off-by: Siddharth Chandrasekaran <siddharth@embedjournal.com>

--- runcon/shell_alias.sh
+++ runcon/shell_alias.sh
@@ -21,23 +21,16 @@ alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo
 
 alias ll='ls -la'
 alias gcc='gcc -Wall'
-alias byebye='sudo shutdown -P now'
-alias py='python3.4'
-alias sl='ls'
-alias pl='perl'
 alias gst='git status'
 alias gdf='git diff'
 alias gdfc='git diff --cached'
-alias gl='git log'
-alias create='create.pl'
-alias cim='vim'
 alias iface='ifconfig -s | sed 1d | cut -d" " -f1'
-alias rm_lines='perl -ne '"'"'`rm -rf $_`'"'"' '
-alias gg='git grep -n'
-alias syslog='sudo tail -f /var/log/kern.log | perl -pe '"'"'s/.*kernel: \[\d+\.\d+\] //;'"'"''
+alias rm_lines=' perl -ne '\'' system("rm -rf $_"); '\'' '
+alias gg='git grep --line-number --perl-regexp'
+alias syslog='sudo tail -f /var/log/kern.log | perl -pe '\'' s/.*kernel: \[\d+\.\d+\] //; '\'' '
 
 # git format patch numbered
 alias gfp_num='git format-patch --numbered --numbered-files'
 # get commit message from git-format-patch files.
-alias gfp_cmsg='perl -ne '"'"'last if (/^---$/); $p=1 if (s/^Subject: (\[.+\] )?//); print if $p'"'"''
+alias gfp_cmsg=' perl -ne '\'' last if (/^---$/); $p=1 if (s/^Subject: (\[.+\] )?//); print if $p '\'' '
 
