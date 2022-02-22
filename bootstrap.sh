181d80d8d80214b2a6201a9961ea4d1856ecdac9
Siddharth Chandrasekaran <sidcha@amazon.de>
2022-02-22 19:19:00 +0100

    Handle the case when git is not not installed

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 if [ ! -d ~/.files ]; then
-	echo "Cloning https://github.com/sidcha/dotfiles.git"
-	git clone https://github.com/sidcha/dotfiles.git $HOME/.files
-else
+	if [ ! command -v git &>/dev/null ]; then
+		echo "Downloading github.com/sidcha/dotfiles master"
+		wget -O /tmp/master.zip https://github.com/sidcha/dotfiles/archive/refs/heads/master.zip
+		unzip -q /tmp/master.zip && rm /tmp/master.zip
+		mv dotfiles-master ~/.files
+	else
+		echo "Cloning github.com/sidcha/dotfiles"
+		git clone https://github.com/sidcha/dotfiles.git ~/.files
+	fi
+fi
+
+if [ -d ~/.files/.git ]; then
 	echo "Updating ~/.files"
 	git -C ~/.files pull origin master
 fi
